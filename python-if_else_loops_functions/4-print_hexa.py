aeb2e6c5c0bb2309d4f7ad2a841a3b4fc1475ce1
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-13 18:49:08 +0000

    4-print_hexa.py

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 for i in range(99):
     print("{} = 0x{:x}".format(i, i))
+
+
+#  for i in range(99) — Loops from 0 to 98.
+#  "{} = 0x{:x}".format(i, i) — Formats:
+#  {} → decimal i
+#  {:x} → hexadecimal i in lowercase
+#  Only one print() is used inside the loop
