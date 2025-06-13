d98b518af54f1c7ea2a150a2da99afaed427eda6
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-13 18:27:06 +0000

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 print("".join("{}".format(chr(i)) for i in range(97, 123)), end="")
-'''range(97, 123) — range of ASCII codes for lowercase English letters (a–z).
-chr(i) — converts ASCII code to character.
-"{}".format(...) — formats each character as specified.
-"".join(...) — concatenates all characters into one string.
-end="" in print() — to avoid adding a line break at the end'''
+#range(97, 123) — range of ASCII codes for lowercase English letters (a–z).
+#chr(i) — converts ASCII code to character.
+#"{}".format(...) — formats each character as specified.
+#"".join(...) — concatenates all characters into one string.
+#end="" in print() — to avoid adding a line break at the en
