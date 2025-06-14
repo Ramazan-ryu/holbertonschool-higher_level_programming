cb3a1381c3b21059ab8df8740d544f1f3f1bb6d6
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-14 16:49:21 +0000

    100-print_tebahpla.py---advanced

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-for i in range(26):
-    print(chr(ord('z') - i) if i % 2 == 0 else chr(ord('Z') - i), end='')
+print("".join(chr(122 - i) if i % 2 == 0 else chr(90 - i) for i in range(26)), end="")
