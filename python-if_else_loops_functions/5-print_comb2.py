50235008063e73b855c046257f59cb3a3bb238e3
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-14 12:11:08 +0000

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(("0" * (i < 10) + "{}".format(i)),
-            end=", " * (i != 99) + "\n" * (i == 99))
+    print(
+     ("0" * (i < 10) + "{}".format(i)),
+     end=", " * (i != 99) + "\n" * (i == 99)
+     )
