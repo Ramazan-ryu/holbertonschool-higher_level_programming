ad5f3d8a1f73f191909372c5a861ef8a8e2deb2e
ramazan_mustafayev <mustafayevramzan121@gmail.com>
2025-06-17 17:22:56 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-def LEN(num, name):
-    print("6 arguments:\n",name+","+num)
-LEN("Hello Welcome To The Best School","0")
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    n = len(argv)
+
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print(f"{n} arguments:")
+
+    for i in range(n):
+        print(f"{i + 1}: {argv[i]}")
