0af6a3ed25dfd3b6db961b6110a063ef5f2561c9
ramazan_mustafayev <mustafayevramzan121@gmail.com>
2025-06-17 14:45:17 +0000

    1-calculation.py/calculation-1.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 from add_0 import add
+#Imports the add function from the file add_0.py
 if __name__ == "__main__":
+#Runs the code block only if the script is executed directly    
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
