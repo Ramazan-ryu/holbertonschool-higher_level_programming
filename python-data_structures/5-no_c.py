9f9ea6ea2703cd6793c30f1664b060d6b8525d6d
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-21 09:18:20 +0000

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     result = ""
     while i < len(my_string):
         char = my_string[i]
-        if char != "C" and char !="c":
+        if char != "C" and char != "c":
             result += char
-        i += 1
+            i += 1
     return result
