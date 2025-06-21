4704a401b1291b430d1e1c63031c895615cf7569
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-21 10:06:29 +0000

    6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for li in matrix:
+        for num in range(len(li)):
+            print("{:d}".format(li[num]), end='')
+            if num != len(li) - 1:
+                print('', end='')
+        print() 
