da30a150c754fe0ebf2b629ff9c055589ede841b
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-21 12:24:33 +0000

    9-max_integer.py/9-main

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_v = my_list[0]
+    for num in my_list:
+        if num > max_v:
+            max_v = num
+    return max_v
