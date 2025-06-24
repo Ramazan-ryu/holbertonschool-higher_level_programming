79089d90eb8815751bf65472238d8a110ce01996
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-24 16:30:10 +0000

    11-multiply_list_map.py//11-main.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     return(list(map(lambda x: x * number, my_list)))
-'''list — converts an iterator (e.g. result map) into a list.
-
-lambda is a small function without a name, such as lambda x: x * number.
-
-return — returns a value from a function.
-
-map — applies a function ( lambda) to each element of a list and returns an iterator with the results.
-
-Concatenation:
-return list(map(lambda x: x * number, my_list))'''
