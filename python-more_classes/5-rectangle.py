e3ea3f241769bf8edcaed0584b3c9f567cd6c842
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-29 21:27:34 +0000

    5-rectangle.py===5-main.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
         return rectangle.rstrip()
 
     def __repr__(self):
-        return (f"Rectangle ({self.__width}, {self.__height})")
+        return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
         return ("Bye rectangle...")
