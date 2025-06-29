ee554241b291431805bd0ded6823bd844f803ada
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-29 23:16:25 +0000

    0-lookup.py===0-main.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,4 +4,9 @@ and methods of an object'''
 
 
 def lookup(obj):
+    """ this function returns the attribute of an object
+    arguments:obj(object)
+    Return:A list of attributes is returned
+    """
+
     return dir(obj)
