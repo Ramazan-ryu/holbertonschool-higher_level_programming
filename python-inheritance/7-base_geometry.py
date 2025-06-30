9ca8c601104d019b804c73058d9fe5dcb982f672
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-30 01:04:41 +0000

    7-base_geometry.py///main.py 77777

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class that is a subclass and initialises an object"""
+
+
+class BaseGeometry:
+    """ a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+    Instantiation with width and height: def __init__(self, width, height)
+    width and height must be private. No getter or setter
+    width and height must be positive integers, validated by integer_validator
+    """
