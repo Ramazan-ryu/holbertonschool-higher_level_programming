1fe2bb4a794a53b87312e369a0b441fbf0dcaf46
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-30 14:43:28 +0000

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,17 +2,18 @@
 '''a class that is a subclass and initialises an object'''
 
 
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-     """a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+    """a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
     Instantiation with width and height: def __init__(self, size)
     size  must be private. No getter or setter
     size  must be positive integers, validated by integer_validator
     """
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
