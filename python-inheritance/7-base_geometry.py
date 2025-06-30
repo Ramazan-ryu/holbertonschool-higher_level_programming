66c8b958b5550f3edd42ce81a1908e2597426a36
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-30 01:12:09 +0000

    7-base_geometry.py///main.py 77777

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""a class that is a subclass and initialises an object"""
+"""creates a class that raises an exception"""
 
 
 class BaseGeometry:
-    """ a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
-    Instantiation with width and height: def __init__(self, width, height)
-    width and height must be private. No getter or setter
-    width and height must be positive integers, validated by integer_validator
-    """
+    """this class raises an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value < 0 or value == 0:
+            raise ValueError(f"{name} must be greater than 0")
