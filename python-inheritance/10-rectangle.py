5d729a5c475fee8c5b500d3bbdd1a41811ca26cd
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-30 02:07:27 +0000

    10-rectangle.py

---
+++ python-inheritance/10-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a class that is a subclass and initialises an object"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+    Instantiation with width and height: def __init__(self, size)
+    size  must be private. No getter or setter
+    size  must be positive integers, validated by integer_validator"""
+    def __init__(self, size):
