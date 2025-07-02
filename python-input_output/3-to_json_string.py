dbc70abbfa733bcbccc0c472d185250d48026ca9
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-02 22:10:52 +0000

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that returns the JSON representation of an object (string):"""
+import json
+
+
+def to_json_string(my_obj):
+    """A function that serialises an object
+    arguments:
+    my_obj: the object to be serialised
+    Return: A JSON string is returned
+    """
+
+    j = json.dump
+    print(j)
