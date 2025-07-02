786f547b137b39c4ee98fbe4c5aa2071fd26131a
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-02 21:27:39 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""a script that reads a text file (UTF8) and prints it to stdout:"""
+
 
 def read_file(filename=""):
-    with open(filename,"o" as file:
-            a=filename.read()
-            print(a)
+    """A function that reads and prints the content of a file to STDOUT
+    Arguments
+    filename:(string) the name of the file or its relative path
+    """
+    with open(filename, "r") as file:
+        a = file.read()
+        print(a)
