510a4b522ddf8a80b18f9edeef78f3e4ff58d148
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-02 23:28:29 +0000

    smthg i hate

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,12 @@ import json
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
 
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
 
-with open("add_item.json", "w") as f:
-    save_to_json_file(argv[1:], "add_item.json")
+items.extend(argv[1:])
+save_to_json_file(items, filename)
