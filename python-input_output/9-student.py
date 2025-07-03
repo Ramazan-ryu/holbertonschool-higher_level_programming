8075814124fb928c9b72dc1ee4bc20978fba976e
ramazan_mustafayev <mustafayev_ramazan12@gmail.com>
2025-07-03 00:01:13 +0000

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,11 +10,10 @@ class Student:
             age
             _json: retrieves a dictionary of the instance attributes
     """
-    def __init__(self,first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         return self.__dict__
-
