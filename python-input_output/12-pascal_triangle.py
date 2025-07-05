dd92a5b563516654c77d7e33fd5a88754b6cddf6
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-05 14:07:37 +0000

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+'''Technical interview preparation'''
+
 
 def pascal_triangle(n):
+    '''Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal's triangle of n
+    Returns an empty list if n <= 0
+    You can assume n will be always an integer
+    You are not allowed to import any module'''
     triangle = []
     for i in range(n):
         if i == 0:
