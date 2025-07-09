c7aad43faefb7e70a477e3fee7aed2d0a73be941
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-09 11:18:54 +0000

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates and fills a table second_table with attributes id, name and score.
-CREATE TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
