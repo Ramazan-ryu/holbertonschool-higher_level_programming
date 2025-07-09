c0051175b04e87d94ff8c3e3a16e595bdac8ce7b
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-09 09:15:32 +0000

    0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-#list_databases
-
-SHOW DATABASES 
+# list_databases
+import mysql.connector
+mysql.connector.connect(
+	host="localhost",
+       	user="root"
+	)
+cursor=connection.cursor
+cursor execute("SHOW DATABASES ")
+for db in cursor: print(db[0])
