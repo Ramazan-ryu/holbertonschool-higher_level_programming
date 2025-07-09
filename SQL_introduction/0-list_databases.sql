fe7c6ea4c9f0a70c538e07bb427404e964a94694
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-09 09:28:32 +0000

    0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,13 +1,2 @@
-#!/usr/bin/python3
-# list_databases
-
-
-import mysql.connector
-connection=mysql.connector.connect(
-	host="localhost",
-       	user="root",
-	password=""
-)
-cursor=connection.cursor()
-cursor.execute("SHOW DATABASES")
-for db in cursor: print(db[0])
+-- Lists all databases of the MySQL server.
+SHOW DATABASES;
