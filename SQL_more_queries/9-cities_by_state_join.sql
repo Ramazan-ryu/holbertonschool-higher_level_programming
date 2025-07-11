01113e5ea6b85d9da4dc3c2ec97519576bae7aaa
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 16:50:29 +0000

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,7 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 USE hbtn_0d_usa;
-SELECT cities.id,cities.name,states.name FROM cities
+SELECT cities.id,cities.name,states.name 
+FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
