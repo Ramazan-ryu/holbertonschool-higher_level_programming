c4438caade00354097675a9debd3aa2f54b4cdf2
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 17:12:38 +0000

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -6,5 +6,5 @@
 SELECT tv_shows.title, tv_show.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC ,BY tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC ,tv_show_genres.genre_id ASC;
 
