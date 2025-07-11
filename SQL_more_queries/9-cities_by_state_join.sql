9a8dbf91dba04d5f568ea6e7bda66761642acd08
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 16:53:17 +0000

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 SELECT cities.id,cities.name,states.name 
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
