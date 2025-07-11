8d65860595560fac9cf3255756cd1288d044cacf
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 18:32:12 +0000

    15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -10,5 +10,5 @@ FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = Comedy
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
