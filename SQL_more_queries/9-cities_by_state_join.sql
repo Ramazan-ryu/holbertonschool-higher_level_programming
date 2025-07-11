a0c95dbbea38a8c15d828aa406cf28b0d384f0f0
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 16:51:43 +0000

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 SELECT cities.id,cities.name,states.name 
 FROM cities
