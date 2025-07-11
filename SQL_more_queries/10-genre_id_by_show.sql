72dd2c4c651ac1282add9ad29fca687b84690dda
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 17:15:14 +0000

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 
-SELECT tv_shows.title, tv_show.genre_id
+SELECT tv_shows.title, tv_show.genres_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC ,tv_show_genres.genre_id ASC;
