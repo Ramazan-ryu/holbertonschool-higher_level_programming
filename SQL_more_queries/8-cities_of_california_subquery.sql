a798861394d5b07bac08fe156e8d0cf6b91bf0a9
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 16:24:35 +0000

    8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,7 +3,7 @@
    -- Results must be sorted in ascending order by cities.id
    -- The database name will be passed as an argument of the mysql command
 SELECT id,name FROM cities
-WHERE states_id=(
+WHERE state_id=(
 	SELECT id FROM states WHERE name="California"
 );
 ORDER BY id ASC;
