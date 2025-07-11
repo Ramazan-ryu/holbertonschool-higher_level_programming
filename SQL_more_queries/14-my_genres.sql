d0f50aab7e30d6e524a098ad348721ae60377004
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-11 18:07:40 +0000

    14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,3 +3,10 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
+
+SELECT tv_genres.name
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = Dexter
+ORDER BY tv_genres.name ASC;
