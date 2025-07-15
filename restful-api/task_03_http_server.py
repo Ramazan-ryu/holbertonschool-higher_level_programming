7f48a8fbfbc065f6bf4451bb361f24116d653d90
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-15 17:04:34 +0000

    task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -14,29 +14,33 @@ class SimpleAPIHeader(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "txt/plain")
             self.end_headers()
             self.wfile.write(b"Hello. This is the simple API!")
-        elif self.data == "/data":
+
+        elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type","txt/plain")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dump(data).encode())
-        elif self.status == "":
+
+        elif self.path == "":
             self.send_response(200)
             self.send_header("Content-type", "txt/plain")
             self.end_headers()
             self.wfile.write(b"OK")
-        elif self.info == "/info":
+
+        elif self.path == "/info":
             self.send_response(200)
             self.send_header("Content-type","txt/plain")
             self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
             self.wfile.write(json.dump(info).encode())
+
         else:
             self.send_response(400)
             self.send_header("Content-type","txt/plain")
             self.end_headers()
             self.wfile.write(b"Endpoint not foun")
+
 with socketserver.TCPServer(("",PORT),SimpleAPIHeader) as httpd:
     print(f"serving port {PORT}")
     httpd.serve_forever()
-        
