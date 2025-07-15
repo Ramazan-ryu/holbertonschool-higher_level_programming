35593170d9d0678c04842e785ab260edc688e908
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-15 11:51:30 +0000

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,7 +4,19 @@ import csv
 
 
 def fetch_and_print_posts():
-    url="https://jsonplaceholder.typicode.com/"
+    url="https://jsonplaceholder.typicode.com/posts"
     a=requests.get(url)
-    print(f"Status Code: 200 {a} ")
-    if url
+    print(f"Status Code: 200 {response.status_code}")
+    if url.status_code==200:
+        for j in a.json():
+            print(j)
+
+def fetch_and_save_posts():
+    url="https://jsonplaceholder.typicode.com/posts"
+    a=requests.get(url)
+    posts=[{"id": post["id"],"title":post["title"],"body":post["body"]} for p in a.json()]
+    with open('posts.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'title', 'body'])
+        for post in posts:
+            writer.writerow([post['id'], post['title'], post['body']])
