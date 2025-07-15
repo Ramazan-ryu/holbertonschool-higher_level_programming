836c48e8b93b91ad7580070289bfe91a0253ee3f
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-15 00:47:44 +0000

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 import requests
-import json 
+import csv
+
+
+def fetch_and_print_posts():
+    url="https://jsonplaceholder.typicode.com/"
+    a=requests.get(url)
+    print(f"Status Code: 200 {a} ")
+    if url
