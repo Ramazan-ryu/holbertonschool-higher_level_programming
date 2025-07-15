0161f7f12a145d126aedaf7df84eb884b1d1efa4
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-15 16:48:04 +0000

    task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,4 +1,6 @@
 #!/usr/binpython3
+"""Python web server"""
+
 import json
 import http.server
 import socketserver
@@ -7,21 +9,24 @@ PORT=8000
 
 class SimpleAPIHeader(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.response(200)
         if self.path == "/":
+            self.response(200)
             self.send_header("Content type", "txt/plain")
             self.end_headers()
             self.wfile.write("Hello. This is the simple API!")
         elif self.data == "/data":
+            self.response(200)
             self.send_header("Content type","txt/plain")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dump(data))
         elif self.status == "":
+            self.response(200)
             self.send_header("Content type", "txt/plain")
             self.end_headers()
             self.wfile.write("OK")
         elif self.info == "/info":
+            self.response(200)
             self.send_header("Content type","txt/plain")
             self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
@@ -31,7 +36,7 @@ class SimpleAPIHeader(http.server.BaseHTTPRequestHandler):
             self.send_header("Content type","txt/plain")
             self.end_headers()
             self.wfile.write("Endpoint not foun")
-        with socketserver.TCPServer(("",PORT),SimpleAPIHeader) as httpd:
-            print("serving port", PORT)
-            httpd.serve_forever()
+with socketserver.TCPServer(("",PORT),SimpleAPIHeader) as httpd:
+    print("serving port", PORT)
+    httpd.serve_forever()
         
