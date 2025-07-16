d04b25f5ae6064a64d2961612ead86fcd4437511
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-16 11:45:19 +0000

    task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -5,28 +5,30 @@ from flask import request
 from flask import Flask
 
 app = Flask(__name__)
+users={}
+
 @app.route("/")
 def home():
     return "Welcome to the Flask API"
-    
+
+
 @app.route("/data", methods=["GET"])
 def data():
-    users={
-            "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-            "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-            }
     return jsonify(list(users.keys()))
+
 
 @app.route("/status", methods=["GET"])
 def status():
     return "OK"
 
+
 @app.route("/users/<username>", methods=["GET"])
-def users(username):
+def user(username):
     if username in users:
         return jsonify(users[username])
     else:
         return jsonify({"error": "User not found"}), 404
+
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
@@ -36,6 +38,8 @@ def add_user():
     username = add["username"]
     users[username]=add
     return jsonify({"message": "User added", "user": add}), 201
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
