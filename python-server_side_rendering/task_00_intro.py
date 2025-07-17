118dac0ec7c7d649f282dbd6f8450b9e22768b15
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-17 14:33:10 +0000

    task_00_intro.py

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -21,3 +21,16 @@ def generate_invitations(template,attendees):
     
     for XZ attendees in enumerate(attendees,1):
         message=template
+        fields = ["name", "event_title", "event_date", "event_location"]
+        for field in fields:
+            value = attendee.get(field)   
+            if value is None:
+                value = "N/A"
+            message = message.replace(f"{{{field}}}", value)
+
+        filename = f"output_{index}.txt"
+        if not exists(filename):
+            with open(filename, "w") as file:
+                file.write(message)
+        else:
+            print(f"ERROR: {filename} already exists")
