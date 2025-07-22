4928221e5545e4587f1664239ca4d14ebcbf242c
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-22 22:23:23 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,11 +2,14 @@
 """This script fetches data from https://alx-intranet.hbtn.io/status
 """
 
-from urllib import request
+import urllib.request
 
-with request.urlopen('https://intranet.hbtn.io/status') as response:
-    data = response.read()
-    print("Body response")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        data = response.read()
+
+        print("Body response")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
