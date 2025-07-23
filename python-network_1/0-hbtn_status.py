7fb0b50d0d8de02599ff2618c9f26cf45e1018d8
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 10:57:25 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""This script fetches data from https://alx-intranet.hbtn.io/status
+"""This script fetches data from
+https://alx-intranet.hbtn.io/status
 """
-
 from urllib import request
-
 if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
     try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-            data = response.read()
+        with urllib.request.urlopen('url') as zapros:
+            data = zapros.read()
             print("Body response")
             print("\t- type: {}".format(type(body)))
             print("\t- content: {}".format(body))
             print("\t- utf8 content: {}".format(body.decode('utf-8')))
-
     except Exception as e:
         print(f"Error ocuured as {e}")
