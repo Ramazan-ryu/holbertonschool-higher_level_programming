a49087fcf63fdc2630cd686ef7fedfe2d13f3426
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 10:47:16 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,11 +5,15 @@
 from urllib import request
 
 if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+            data = response.read()
+            print("Body response")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode('utf-8')))
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        data = response.read()
+    except Exception as a:
+        print("Error ocuured as {a}")
 
-        print("Body response")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        
