4ac86027303447b7320608769712545814d6d662
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 13:10:34 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -11,9 +11,9 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = parse.urlencode({"email": email}).encode("utf-8")
-    req = request.Request(url, data=data)
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    req = urllib.request.Request(url, data=data)
 
-    with request.urlopen(req) as response:
+    with urllib.request.urlopen(req) as response:
         body = response.read().decode("utf-8")
         print(body)
