38bc9756075c6ebc20e2ede7edeb979b6499c3c1
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 15:04:47 +0000

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email address, sends a POST request 
-to the passed URL with the email as a parameter, and finally 
-displays the body of the response.
-
+script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally
+displays the body of the response.]]]]]]]]]]]]]]]]]]]]]]
 The email must be sent in the variable email
 You must use the packages requests and sys
 You are not allowed to import packages other than requests and sys
 You dont need to error check arguments passed to the script (number or type)
-Please test your script in the container provided, using the web server running on port 5000"""
+Please test your script in the container provided, using the web server
+running on port 5000
+"""
 import requests
 import sys
 if __name__ == "__main__":
@@ -17,5 +18,4 @@ if __name__ == "__main__":
 
     data = {'email': email}
     response = requests.post(url, data=data)
-    
     print(response.text)
