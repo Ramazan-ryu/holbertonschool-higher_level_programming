dbb23eeb5a03e2041369ff9952c935048bda6a9c
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 12:39:32 +0000

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as zapros:
         head = zapros.headers
-        zapros-id-x = headers.get('X-Request-Id')
-        print(zapros-id-x)
+        zapros-r_id-x = headers.get('X-Request-Id')
+        print(zapros-r_id-x)
