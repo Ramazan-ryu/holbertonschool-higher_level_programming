853d2dae99b5d45ccbd16b6c9c8710891c2fd59f
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 14:14:10 +0000

    4-hbtn_status.py

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+
+You must use the package requests
+You are not allow to import packages other than requests
+The body of the response must be display like the following example (tabulation before -)
+"""
+
+import requests
+if __name__ == "__main__":
+    url = request.get('https://intranet.hbtn.io/status')
+    body = url.txt
+    print("Body response")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
