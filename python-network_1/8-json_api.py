7f6dbfe254a8c782b1bc2e47b37c62563c23a21b
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 15:40:53 +0000

    8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""This script sends a POST request with a search letter and processes the JSON response."""
-# Import modules
+"""This script sends a POST request with a search letter
+and processes the JSON response."""
+"""Import modules"""
+
 import requests
 import sys
-# Get the letter (if given)
+"""Get the letter (if given)"""
 if len(sys.argv) > 1:
-    q = sys.argv[1]  # Take the first argument
+    q = sys.argv[1]  """Take the first argument"""
 else:
-    q = ""           # If nothing is given, use empty string
-# Prepare the POST request
+    q = ""           """If nothing is given, use empty string"""
+"""Prepare the POST request"""
 url = "http://0.0.0.0:5000/search_user"
 data = {'q': q}
-# Send the POST request and get the response
+"""Send the POST request and get the response"""
 response = requests.post(url, data=data)
-# Try to convert the response to JSON
+"""Try to convert the response to JSON"""
 try:
     json_data = response.json()
     if json_data:
