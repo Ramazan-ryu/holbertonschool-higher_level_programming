a9ffd0279002e5ba5dbf04ed41f8ab249e4fec42
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 14:56:36 +0000

    5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -12,5 +12,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    x_id = response.header.get('X-Request-Id')
+    x_id = response.headers.get('X-Request-Id')
     print(x_id)
