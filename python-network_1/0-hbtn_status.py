28800943fe3c5d9f920b6c330bc3a60fd3af7c19
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 16:03:40 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ from urllib import request
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     try:
-        with request.urlopen(url) as zapros:
-            body = zapros.read()
+        with request.urlopen(url) as response:
+            body = response.read()
             print("Body response")
             print("\t- type: {}".format(type(body)))
             print("\t- content: {}".format(body))
