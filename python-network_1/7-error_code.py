244b594e62c69a26d6532ad18963a2f03a269f8f
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-23 15:14:34 +0000

    7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -13,7 +13,7 @@ import requests
 import sys
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = request.get(url)
+    response = requests.get(url)
     if response.status_code >= 400:
         print("Error code:", response.status_code)
     else:
